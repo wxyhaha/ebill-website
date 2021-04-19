6ab3d647126c7af51a3ebce961c243c9c4fa5827
joel <1666464510@qq.com>
2021-04-19 15:35:20 +0800

    deploy

---
+++ precache-manifest.c10e1abf97ac79e8dd58028014c9ba49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c682891bb938fa04518",
+    "url": "/ebill-website/css/app.3545278e.css"
+  },
+  {
+    "revision": "364dcab81c1821e69e29a4cb402b889f",
+    "url": "/ebill-website/index.html"
+  },
+  {
+    "revision": "8c682891bb938fa04518",
+    "url": "/ebill-website/js/app.c2116a10.js"
+  },
+  {
+    "revision": "054b4747ed1b3294dc08",
+    "url": "/ebill-website/js/chunk-vendors.856a950f.js"
+  },
+  {
+    "revision": "a0f3155c1fe12b0e44e6a41798b851fd",
+    "url": "/ebill-website/manifest.json"
+  },
+  {
+    "revision": "07764844c4fb70f2f93d7440476c5e2b",
+    "url": "/ebill-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ebill-website/robots.txt"
+  }
+]);
